7c291c0cd7f1fba670c68ee7a6d55a22d53734c2
endalk200 <eb808826@gmail.com>
2024-09-21 22:22:00 +0300

    feat: adds login and me services that uses jwtService for verification
    - uses ECDSA algorithim for jwt generation and verification
    - adds me service which takes in userId and returns user information

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,26 @@ import { PasswordService } from "./password.service";
 import { ConfigModule } from "@nestjs/config";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
+import * as fs from "fs";
+import * as path from "path";
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    JwtModule.register({
+      global: true,
+      privateKey: fs.readFileSync(path.join(__dirname, "../../ec-private.pem")),
+      publicKey: fs.readFileSync(path.join(__dirname, "../../ec-public.pem")),
+      signOptions: {
+        algorithm: "ES256", // Use ES256 for elliptic curve signing
+        expiresIn: "1h", // Token expiration time
+      },
+      verifyOptions: {
+        algorithms: ["ES256"], // Ensure ES256 is used for verification
+      },
+    }),
+  ],
   providers: [PasswordService, AuthService],
   controllers: [AuthController],
   exports: [PasswordService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,45 +1,115 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { PasswordService } from "./password.service";
 import { TsRestException } from "@ts-rest/nest";
 import { authContract } from "./contracts/auth.contract";
+import { safeAwait } from "src/utils/safe-await";
+import { JwtService } from "@nestjs/jwt";
+
+export type JWTPayload = {
+  sub: string;
+  iss: string;
+  aud: string;
+  iat: number;
+  exp: number;
+};
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+
   constructor(
     protected readonly prisma: PrismaService,
     protected readonly passwordService: PasswordService,
+    private jwtService: JwtService,
   ) {}
 
   async login(data: { email: string; password: string }) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email: data.email,
-      },
-    });
-
-    if (!user) {
-      throw new TsRestException(authContract.login, {
-        status: 400,
+    const [user, error] = await safeAwait(
+      this.prisma.user.findUniqueOrThrow({
+        where: {
+          email: data.email,
+        },
+      }),
+    );
+    if (error != null) {
+      this.logger.error(
+        `User table db query failed. ERROR: ${JSON.stringify(error)}`,
+      );
+      throw new TsRestException(authContract.me, {
+        status: 401,
         body: {
-          code: "InvalidCredentials",
-          message:
-            "Invalid credentials provided, please try again with correct credentials.",
+          message: "Unauthorized",
+        },
+      });
+    }
+
+    if (!user.isActive) {
+      this.logger.debug(
+        `User: isActive: ${user.isActive} isEmailVerified: ${user.isEmailVerified}`,
+      );
+      throw new TsRestException(authContract.login, {
+        status: 423,
+        body: {
+          message: `The account is not active, ${!user.isEmailVerified ? "Please verify your email address" : "Please contact admin"} to activate your account`,
         },
       });
     }
 
     if (await this.passwordService.compare(data.password, user.password)) {
-      return user;
+      const payload = {
+        sub: user.id,
+        iss: "backend",
+        aud: "backend",
+      };
+
+      const accessToken = this.jwtService.sign(payload);
+
+      return {
+        auth: {
+          token: accessToken,
+        },
+        user: user,
+      };
     }
 
     throw new TsRestException(authContract.login, {
       status: 400,
       body: {
-        code: "InvalidCredentials",
         message:
           "Invalid credentials provided, please try again with correct credentials.",
       },
     });
+  }
+
+  async me(userId: string) {
+    const [user, error] = await safeAwait(
+      this.prisma.user.findUniqueOrThrow({
+        where: {
+          id: userId,
+        },
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          isEmailVerified: true,
+          isActive: true,
+        },
+      }),
+    );
+    if (error != null) {
+      this.logger.error(
+        `User table db query with id [${userId}] failed. ERROR: ${JSON.stringify(error)}`,
+      );
+      throw new TsRestException(authContract.me, {
+        status: 401,
+        body: {
+          message: "Unauthorized",
+        },
+      });
+    }
+
+    return user;
   }
 }
